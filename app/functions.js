689fc6f44b9cd1f47e1131f149ecddff787310e0
Mazdak Momen <mazdak.momen@gmail.com>
2015-03-10 14:49:18 -0700

    you should be able to use an array as arguments when calling a function - brute force

--- app/functions.js
+++ app/functions.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn(arr[0], arr[1], arr[2]);
     },
 
     speak : function(fn, obj) {
